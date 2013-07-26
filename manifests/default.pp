1ee12012b39c0ae305de6226c0831357a1efe2ad
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-26 17:51:43 +0200

    First commit

---
+++ manifests/default.pp
@@ -0,0 +1,154 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+apt::source { 'packages.dotdeb.org':
+  location          => 'http://packages.dotdeb.org',
+  release           => $lsbdistcodename,
+  repos             => 'all',
+  required_packages => 'debian-keyring debian-archive-keyring',
+  key               => '89DF5277',
+  key_server        => 'keys.gnupg.net',
+  include_src       => true
+}
+
+if $lsbdistcodename == 'squeeze' {
+  apt::source { 'packages.dotdeb.org-php54':
+    location          => 'http://packages.dotdeb.org',
+    release           => 'squeeze-php54',
+    repos             => 'all',
+    required_packages => 'debian-keyring debian-archive-keyring',
+    key               => '89DF5277',
+    key_server        => 'keys.gnupg.net',
+    include_src       => true
+  }
+}
+
+package { 'apache2-mpm-prefork':
+  ensure => 'installed',
+  notify => Service['apache'],
+}
+
+file { '/home/vagrant/.bash_aliases':
+  ensure => 'present',
+  source => 'puppet:///modules/puphpet/dot/.bash_aliases',
+}
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git',
+    'lynx',
+    'gearman'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'inventory.dev':
+  server_name   => 'inventory.dev',
+  serveraliases => [
+],
+  docroot       => '/vagrant/',
+  port          => '80',
+  env_variables => [
+],
+  priority      => '1',
+}
+
+class { 'php':
+  service       => 'apache',
+  module_prefix => '',
+  require       => Package['apache'],
+}
+
+php::module { 'php5-mysql': }
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-gd': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+#php::module { 'php-apc': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+
+php::pecl::module { 'gearman':
+  use_package => false,
+}
+
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "Europe/Paris"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+
+class { 'mysql::server':
+  config_hash   => { 'root_password' => 'myc-sense' }
+}
+
+mysql::db { 'myc-sense':
+  grant    => [
+    'ALL'
+  ],
+  user     => 'myc-sense',
+  password => 'myc-sense',
+  host     => 'localhost',
+  charset  => 'utf8',
+  require  => Class['mysql::server'],
+}
+
+class { 'phpmyadmin':
+  require => [Class['mysql::server'], Class['mysql::config'], Class['php']],
+}
+
+apache::vhost { 'phpmyadmin':
+  server_name => 'phpmyadmin',
+  docroot     => '/usr/share/phpmyadmin',
+  port        => 80,
+  priority    => '10',
+  require     => Class['phpmyadmin'],
+}
+
