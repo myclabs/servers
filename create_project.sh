7fd3808021daeec83bcd689112d08c61a1132b6f
Matthieu Napoli <matthieu@mnapoli.fr>
2014-02-10 10:34:29 +0100

    Restoration de la config Supervisor pour le worker

--- create_project.sh
+++ create_project.sh
@@ -54,6 +54,18 @@ chmod 777 data/proxies
 chmod -R 777 public/cache
 chmod 777 public/temp
 
+cat > /etc/supervisor/conf.d/$NAME-worker.conf <<CONF
+[program:$NAME-worker]
+command=/usr/bin/php /home/web/$NAME/scripts/jobs/work/work.php
+
+; redirige la sortie d'erreur vers stdout
+redirect_stderr=true
+
+stdout_logfile=/home/web/$NAME/data/logs/worker.log
+
+autorestart=true
+CONF
+
 # Logs
 ln -s /home/web/$NAME/data/logs/error.log /etc/logs/logs/$NAME-web.log
 ln -s /home/web/$NAME/data/logs/worker.log /etc/logs/logs/$NAME-worker.log
